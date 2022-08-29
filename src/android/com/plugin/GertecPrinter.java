be3667ca1cf618c9eda73982a3756e38fe26ad1c
AndersonAlvarenga <andersonalvarenga15@gmail.com>
2022-08-29 15:43:37 -0300

    Retirado referencia as lib gertec

--- src/android/com/plugin/GertecPrinter.java
+++ src/android/com/plugin/GertecPrinter.java
@@ -1,52 +1,10 @@
 package com.plugin;
 
-import android.app.Activity;
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Paint;
-import android.graphics.Typeface;
-
-
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.MultiFormatWriter;
-import com.google.zxing.WriterException;
-import com.google.zxing.common.BitMatrix;
-import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 
 public class GertecPrinter {
-  // Definições
-    private final String IMPRESSORA_ERRO = "Impressora com erro.";
-
-    // Statics
-    private static boolean isPrintInit = false;
-
-    // Vaviáveis iniciais
-    private Activity activity;
-    private Context context;
-
-    // Classe de impressão
-    private IGEDI iGedi = null;
-    private IPRNTR iPrint = null;
-    private GEDI_PRNTR_st_StringConfig stringConfig;
-    private GEDI_PRNTR_st_PictureConfig pictureConfig;
-    private GEDI_PRNTR_e_Status status;
-    //Variaval beep
-    private IAUDIO iAudio;
-
-    //Variaveis Led
-    private ILED iLed;
-    private ICL iCl;
-
-    //Variaveis Pagamento
-    private ISMART iSmart;
-    private StringBuilder sb;
-
-    // Classe de configuração da impressão
-    private ConfigPrint configPrint;
-    private Typeface typeface;
-
+  
+   
 
     /**
     * Método construtor da classe usando o GPOS 700
