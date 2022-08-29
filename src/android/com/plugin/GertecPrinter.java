4bb54e9252ae4ed1258bbaedc2f5e5f88f6ea52c
AndersonAlvarenga <andersonalvarenga15@gmail.com>
2022-08-29 16:00:24 -0300

    Fazendo correçoes para publicar app

--- src/android/com/plugin/GertecPrinter.java
+++ src/android/com/plugin/GertecPrinter.java
@@ -1,15 +1,26 @@
 package com.plugin;
 
-
+import android.app.Activity;
+import android.content.Context;
 
 public class GertecPrinter {
-     /**
+  // Definições
+    private final String IMPRESSORA_ERRO = "Impressora com erro.";
+
+    // Statics
+    private static boolean isPrintInit = false;
+
+    // Vaviáveis iniciais
+    private Activity activity;
+    private Context context;
+
+
+    /**
     * Método construtor da classe usando o GPOS 700
     * @param c = Context  atual que esta sendo inicializada a class
     **/
     public GertecPrinter(Context c) {
         this.context = c;
-       
     }
 
 
