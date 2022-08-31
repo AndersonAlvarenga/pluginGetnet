c4fec8110d4c37235b325bbaee55d0680e9af9b5
AndersonAlvarenga <andersonalvarenga15@gmail.com>
2022-08-31 09:53:32 -0300

    Implementacao para testar resposta plugin

--- src/android/com/plugin/GertecPrinter.java
+++ src/android/com/plugin/GertecPrinter.java
@@ -14,8 +14,12 @@ import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.journeyapps.barcodescanner.BarcodeEncoder;
 
+
 public class GertecPrinter {
   
+    private Activity activity;
+    private Context context;
+
     /**
     * Método construtor da classe usando o GPOS 700
     * @param c = Context  atual que esta sendo inicializada a class
@@ -92,130 +96,7 @@ public class GertecPrinter {
       
     }
 
-    /**
-     * Método que recebe o atual texto e ser o mesmo será impresso em negrito e/ou itálico.
-     *
-     * @param texto  = Texto que será impresso.
-     * @param negrito = Caso o texto deva ser impresso em negrito
-     * @param italico  = Caso o texto deva ser impresso em itálico
-     *
-     * @throws Exception = caso a impressora esteja com erro.
-     *
-     * @apiNote = Esse mátodo só altera o tamanho do texto na impressão que for chamado
-     *      * a classe {@link ConfigPrint} não será alterada para continuar sendo usado na impressão da
-     *      * proxíma linha
-     *
-     * */
-    public void imprimeTexto(String texto, boolean negrito, boolean italico) throws Exception {
 
-    }
-    /**
-     * Método que recebe o atual texto e ser o mesmo será impresso em negrito, itálico e/ou  sublinhado.
-     *
-     * @param texto  = Texto que será impresso.
-     * @param negrito = Caso o texto deva ser impresso em negrito
-     * @param italico  = Caso o texto deva ser impresso em itálico
-     * @param sublinhado   = Caso o texto deva ser impresso em itálico.
-     *
-     * @throws Exception = caso a impressora esteja com erro.
-     *
-     * @apiNote = Esse mátodo só altera o tamanho do texto na impressão que for chamado
-     *      * a classe {@link ConfigPrint} não será alterada para continuar sendo usado na impressão da
-     *      * proxíma linha
-     *
-     * */
-    public void imprimeTexto(String texto, boolean negrito, boolean italico, boolean sublinhado) throws Exception {
-
-    }
-
-    /**
-     * Método que faz a impressão de imagens
-     *
-     * @param imagem = Nome da imagem que deve estar na pasta drawable
-     *
-     * @throws IllegalArgumentException = Argumento passado ilegal
-     * @throws GediException = retorna o código do erro.
-     *
-     * */
-    public boolean imprimeImagem( String imagem ) throws GediException {
-        return true;
-    }
-
-    /**
-     * Método que faz a impressão de código de barras
-     *
-     * @param texto = Texto que será usado para a impressão do código de barras
-     * @param height  = Tamanho
-     * @param width  = Tamanho
-     * @param barCodeType  = Tipo do código que será impresso
-     *
-     * @throws IllegalArgumentException = Argumento passado ilegal
-     * @throws GediException = retorna o código do erro.
-     *
-     * */
-    public boolean imprimeBarCode( String texto, int height, int width,  String barCodeType ) throws GediException {
-        return true;
-    }
-
-    /**
-     * Método que faz a impressão de código de barras
-     *
-     * @param texto = Texto que será usado para a impressão do código de barras
-     * @param height  = Tamanho
-     * @param width  = Tamanho
-     * @param barCodeType  = Tipo do código que será impresso
-     *
-     * @throws IllegalArgumentException = Argumento passado ilegal
-
-     *
-     * */
-    public boolean imprimeBarCodeIMG( String texto, int height, int width,  String barCodeType ) throws WriterException {
-
-        return true;
-
-    }
-
-    
-    /**
-     * Método que faz a inicialização da impressao
-     *
-     * @throws Exception = retorno o código do erro.
-     *
-     * */
-    public void ImpressoraInit() throws Exception {
-        
-    }
-
-    /**
-     * Método que faz a finalizacao do objeto iPrint
-     *
-     * @throws GediException = retorno o código do erro.
-     *
-     * */
-    public void ImpressoraOutput() throws GediException {
-        
-    }
-
-    //MetodosTesting
-    public String checkISmart(){
-       
-        return "CheckIsmart";
-    }
-
-    //Metodo Testados e funcionando corretamente
-
-    //-------------------Metodos Led------------------
-     /**
-     * Método Acender o led
-     *
-     *
-     * @return String
-     *
-     * */
-    public String led() {
-
-        return "led";
-    }  
     //----------------Fim metodos Led------------------
      /**
      * Método que retorna o atual estado da impressora
